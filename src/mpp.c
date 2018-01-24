a40f5886c6b813e14cfa6d161b68d6c99f950461
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-01-24 16:30:53 +0100

    Initial commit

---
+++ src/mpp.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : mpp.c
+ Author      : MarvNet
+ Version     :
+ Copyright   : © 2018 MarvNet
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
