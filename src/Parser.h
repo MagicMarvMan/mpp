76b1c7cc4e3a1ea99913b9d2720d85675d8cf602
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-01-24 22:56:18 +0100

    Added startsWith function

--- src/Parser.h
+++ src/Parser.h
@@ -10,4 +10,11 @@
 
 #include <Functions.h>
 
+bool startsWith(const char *pre, const char *str)
+{
+    size_t lenpre = strlen(pre),
+           lenstr = strlen(str);
+    return lenstr < lenpre ? false : strncmp(pre, str, lenpre) == 0;
+}
+
 #endif /* PARSER_H_ */
